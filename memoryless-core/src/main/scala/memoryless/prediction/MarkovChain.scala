b1c5e6c857cc0f83b4dea5a5986280c110fd157c
gcherian <george.vengal@gmail.com>
2017-02-05 09:00:00 -0500

    first commit

--- memoryless-core/src/main/scala/memoryless/prediction/MarkovChain.scala
+++
@@ -1,9 +0,0 @@
-package memoryless.prediction
-
-import Array._
-class MarkovChain(val dimension:Int, val featureLength:Int)(val threshold:Double = 0.95) {
-
-  val transition :Array[Array[Array[Double]]] = ofDim(dimension,featureLength,featureLength)
-
-
-}
