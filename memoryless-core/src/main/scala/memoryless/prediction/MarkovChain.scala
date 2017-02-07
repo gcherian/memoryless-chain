e6e2f51418b7a2afb928ca7498f723c52c6d40fd
gcherian <george.vengal@gmail.com>
2017-02-07 06:42:19 -0500

    Memoryless Chain - Add a train method

--- memoryless-core/src/main/scala/memoryless/prediction/MarkovChain.scala
+++ memoryless-core/src/main/scala/memoryless/prediction/MarkovChain.scala
@@ -1,9 +1,73 @@
 package memoryless.prediction
 
-import Array._
-class MarkovChain(val dimension:Int, val featureLength:Int)(val threshold:Double = 0.95) {
+import keyless.api.Procedure
+import keyless.api.hash.HashableFunction
+import keyless.index.{NonUniqueIndex, FullUniqueIndex}
 
-  val transition :Array[Array[Array[Double]]] = ofDim(dimension,featureLength,featureLength)
+import Array._
+case class Data(dimension:Int,feature:Int)
+class MarkovChain(val dimension:Int, val featureVariations:Int)(val threshold:Double = 0.95) {
+
+
+
+  import java.util.function.{Function ⇒ JFunction, Predicate ⇒ JPredicate}
+
+
+  implicit def toJavaFunction[A, B](f: Function1[A, B]) = new JFunction[A, B] {
+    override def apply(a: A): B = f(a)
+  }
+
+
+  implicit def toJavaPredicate[A](f: Function1[A, Boolean]) = new JPredicate[A] {
+    override def test(a: A): Boolean = f(a)
+  }
+
+
+  val transition :Array[Array[Array[Double]]] = ofDim(dimension,featureVariations,featureVariations)
+
+  def train(history:FullUniqueIndex[Data]) =
+  {
+    val rewrite:NonUniqueIndex[Data] = new NonUniqueIndex( new HashableFunction((d:Data)=>d.dimension),history.getStrategy)
+    history.foreach(new Procedure[Data] {
+      override def execute(o: scala.Any): Boolean = {
+        if (o != null && o.isInstanceOf[Data])
+        rewrite.put(o.asInstanceOf[Data])
+        return true
+      }
+    })
+    rewrite.foreach( new Procedure[Data] {
+      var previousFeature = -1
+      var previousDimension =0
+
+      override def execute(o: scala.Any): Boolean = {
+
+        if (o.isInstanceOf[Data]) {
+
+          var currentFeature = o.asInstanceOf[Data].feature
+          var currentDimension = o.asInstanceOf[Data].dimension
+          transition(dimension)(currentFeature)(currentFeature)=1
+        } else if (o.isInstanceOf[FullUniqueIndex[Data]]) {
+          val fui:FullUniqueIndex[Data] = o.asInstanceOf[FullUniqueIndex[Data]]
+          fui.foreach(new Procedure[Data] {
+            override def execute(o: scala.Any): Boolean = {
+              if (o.isInstanceOf[Data]) {
+                val data = o.asInstanceOf[Data]
+                if (previousFeature>=0) {
+                  transition(data.dimension)(previousFeature)(data.feature)+=1
+                }
+                previousFeature = data.feature
+              }
+              return true
+            }
+          })
+
+
+        }
+        return true
+      }
+    })
+
+  }
 
 
 }
---
+++ memoryless-core/src/test/scala/memoryless/prediction/MarkovChainSuite.scala
@@ -0,0 +1,40 @@
+package memoryless.prediction
+
+import keyless.index.FullUniqueIndex
+import org.scalatest.FunSuite
+
+
+
+/**
+  * Created by gcherian on 2/7/2017.
+  */
+class MarkovChainSuite extends FunSuite {
+
+
+
+  import java.util.function.{Function ⇒ JFunction, Predicate ⇒ JPredicate}
+
+
+  implicit def toJavaFunction[A, B](f: Function1[A, B]) = new JFunction[A, B] {
+    override def apply(a: A): B = f(a)
+  }
+
+
+  implicit def toJavaPredicate[A](f: Function1[A, Boolean]) = new JPredicate[A] {
+    override def test(a: A): Boolean = f(a)
+  }
+
+
+  test("train") {
+    val chain:MarkovChain = new MarkovChain(3,4)(0.95)
+    val fui:FullUniqueIndex[Data] = new FullUniqueIndex[Data]((d:Data)=>d.dimension + ":" + d.feature)
+
+    val d1 = new Data(0,0)
+    val d2 = new Data(0,1)
+    val d3 = new Data(1,0)
+    val d4 = new Data(1,1)
+    fui.put(d1) ; fui.put(d2);fui.put(d3);fui.put(d4)
+    chain.train(fui)
+  }
+
+}
